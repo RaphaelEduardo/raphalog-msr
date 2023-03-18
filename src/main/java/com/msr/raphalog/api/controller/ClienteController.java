e01ae8e40a9712e040784e5b78a321e5f5a2f60a
Raphael Eduardo <raphaeleduardosc@gmail.com>
2023-03-18 15:59:26 -0300

    Validanto com Jakarta Bean Validation

--- src/main/java/com/msr/raphalog/api/controller/ClienteController.java
+++ src/main/java/com/msr/raphalog/api/controller/ClienteController.java
@@ -2,6 +2,8 @@ package com.msr.raphalog.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -51,12 +53,12 @@ public class ClienteController {
 	}
 	
 	@PostMapping @ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente) {
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 	
 	@PutMapping("/{id}")
-	public ResponseEntity<Cliente> atualizar(@PathVariable Integer id, @RequestBody Cliente cliente) {
+	public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Integer id, @RequestBody Cliente cliente) {
 		if (!clienteRepository.existsById(id)) {
 			return ResponseEntity.notFound().build();
 		}
--- src/main/java/com/msr/raphalog/domain/model/Cliente.java
+++ src/main/java/com/msr/raphalog/domain/model/Cliente.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -20,10 +23,14 @@ public class Cliente {
 	@EqualsAndHashCode.Include
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@NotBlank @Size(max=60)
 	private String nome;
+	
+	@NotBlank @Email @Size(max=255)
 	private String email;
 	
-	@Column(name="fone")
+	@NotBlank @Size(max=20) @Column(name="fone")
 	private String telefone;
 	
 	
