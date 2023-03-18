a1eb45abae70af3b9b40b7cd78159c7741bfd339
Raphael Eduardo <raphaeleduardosc@gmail.com>
2023-03-18 15:39:42 -0300

    Implementando o CRUD de cliente

--- src/main/java/com/msr/raphalog/api/controller/ClienteController.java
+++ src/main/java/com/msr/raphalog/api/controller/ClienteController.java
@@ -2,7 +2,16 @@ package com.msr.raphalog.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.msr.raphalog.domain.model.Cliente;
@@ -12,15 +21,58 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/clientes") // Para não precisar mais ficar especificando nos metodos.
 public class ClienteController {
 
 	private ClienteRepository clienteRepository;
 
-	@GetMapping("/clientes")
-	public List<Cliente> listar() {
+	@GetMapping
+	public List<Cliente> listarClientes() {
 		return clienteRepository.findAll();
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> buscarPorId(@PathVariable Integer id) {
+		return clienteRepository.findById(id)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+		
+		/*
+		return clienteRepository.findById(id) // Enxugando o código
+				.map(cliente -> ResponseEntity.ok(cliente))
+				.orElse(ResponseEntity.notFound().build());
+		
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		if (cliente.isPresent()) {  //tem alguma coisa ai dentro desse Optional? (RETORNANDO O STATUS CORRETO) 
+			return ResponseEntity.ok(cliente.get()); // Quando existe um cliente
+		} else {
+			return ResponseEntity.notFound().build(); // Quando não existe.
+		} */
+	}
+	
+	@PostMapping @ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Integer id, @RequestBody Cliente cliente) {
+		if (!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		cliente.setId(id); // Força a atualização, para que não se crie um novo objeto
+		cliente = clienteRepository.save(cliente);
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> remover(@PathVariable Integer id) { // como não precisa retornar nada após a remoção: coloca-se o void no ResponseEntity<>
+		if (!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		clienteRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
 	
 	
 }
