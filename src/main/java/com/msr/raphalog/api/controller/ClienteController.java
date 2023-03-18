807469c46003bdc286c4d750f2d0c87c73dbb8c8
Raphael Eduardo <raphaeleduardosc@gmail.com>
2023-03-18 12:46:19 -0300

    Utilizando Spring Data JPA

--- src/main/java/com/msr/raphalog/api/controller/ClienteController.java
+++ src/main/java/com/msr/raphalog/api/controller/ClienteController.java
@@ -2,25 +2,23 @@ package com.msr.raphalog.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.msr.raphalog.domain.model.Cliente;
+import com.msr.raphalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 
-	@PersistenceContext
-	private EntityManager manager;
-	
+	private ClienteRepository clienteRepository;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		// "from Cliente" é uma JPQL é uma linguagem de consultas do Jarkarta Persistence (parecida com o SQL, mas para consultar no modelo OO).
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList(); // Retorna uma lista de Cliente
+		return clienteRepository.findAll();
 	}
 	
 	
--- src/main/java/com/msr/raphalog/domain/repository/ClienteRepository.java
+++ src/main/java/com/msr/raphalog/domain/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.msr.raphalog.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,9 @@ import com.msr.raphalog.domain.model.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
+	
+	List<Cliente> findByNome(String nome); //retorna por nome
+	List<Cliente> findByNomeContaining(String nome); // retorna por caracteres contidos no nome
 	
 	
 }
