3b0397da4252edda2ff3ca33dc13a3811455a327
Raphael Eduardo <raphaeleduardosc@gmail.com>
2023-03-20 13:33:27 -0300

    Simplificando a transformação de objetos com ModelMapper

--- src/main/java/com/msr/raphalog/domain/model/Entrega.java
+++ src/main/java/com/msr/raphalog/domain/model/Entrega.java
@@ -11,14 +11,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.groups.ConvertGroup;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
-import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
-import com.msr.raphalog.domain.ValidationGroups;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -32,28 +24,19 @@ public class Entrega {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private Integer id;
 	
-	@Valid
-	@ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class)
-	@NotNull 
 	@ManyToOne
 	private Cliente cliente;
 	
-	@Valid
-	@NotNull
 	@Embedded
 	private Destinatario destinatario;
 	
-	@NotNull
 	private BigDecimal taxa;
 	
-	@JsonProperty(access= Access.READ_ONLY)
 	@Enumerated(EnumType.STRING)
 	private StatusEntrega status;
 	
-	@JsonProperty(access= Access.READ_ONLY)
 	private OffsetDateTime dataPedido;
 	
-	@JsonProperty(access= Access.READ_ONLY)
 	private OffsetDateTime dataFinalizacao;
 	
 }
